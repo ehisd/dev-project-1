433a3b9c1f9f6cdd435bb0e9fa189dc439495c48
bhrayhaw <elijahapreko@gmail.com>
2024-02-28 14:36:58 +0000

    resolved linting issues

--- server/src/models/users.js
+++ server/src/models/users.js
@@ -1,21 +1,21 @@
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require("@prisma/client");
+
 const prisma = new PrismaClient();
 
-async function getUsers(){
-    return await prisma.User.findMany();
-
+async function getUsers() {
+  return prisma.user.findMany(); // Remove redundant await
 }
 
 async function main() {
-    const users = await getUsers();
-    console.log(users);
+  const users = await getUsers();
+  console.log(users);
 }
 
 main()
-    .catch((error) => {
-        console.error(error);
-        process.exit(1);
-    })
-    .finally(async () => {
-        await prisma.$disconnect();
-    });
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
