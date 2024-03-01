4c578c0a483aa0c8ac03f0950aacf5fb7fce202e
lawalalx <lawal.alx@gmail.com>
2024-03-01 16:33:47 +0100

    test

--- server/src/controllers/usersController.js
+++ server/src/controllers/usersController.js
@@ -1,5 +1,5 @@
 const { PrismaClient } = require('@prisma/client');
-const { hashPassword, comparePassword } = require('../middlewares/auth.js');
+const { hashPassword, comparePassword } = require('../middlewares/auth.');
 
 const prisma = new PrismaClient({
   log: ['error'],
