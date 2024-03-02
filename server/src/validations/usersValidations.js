fc4bd322b355ebfdaa84a07f295cf3617994ee73
bhrayhaw <elijahapreko@gmail.com>
2024-03-02 18:17:43 +0000

    Added regEx to check the password

--- server/src/validations/usersValidations.js
+++ server/src/validations/usersValidations.js
@@ -6,13 +6,29 @@ const validateUser = Joi.object({
   lastname: Joi.string().required(),
   username: Joi.string().alphanum().min(3).max(30).required(),
   email: Joi.string().email().required(),
-  password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{8,30}$')).required(),
+  password: Joi.string()
+    .min(8)
+    .max(40)
+    .pattern(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&*\()_+{}|:"<>?~`\-=[\]\\;',./]).{8,40}$/
+    )
+    .message(
+      '"{#label}" must contain at least 1 lowercase letter, 1 uppercase letter, 1 number, 1 symbol, and be 8-40 characters.'
+    ),
 });
 
 // validate schema for user login
 const validateLogin = Joi.object({
   email: Joi.string().email().required(),
-  password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{8,30}$')).required(),
+  password: Joi.string()
+    .min(8)
+    .max(40)
+    .pattern(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&*\()_+{}|:"<>?~`\-=[\]\\;',./]).{8,40}$/
+    )
+    .message(
+      '"{#label}" must contain at least 1 lowercase letter, 1 uppercase letter, 1 number, 1 symbol, and be 8-40 characters.'
+    ),
 });
 
 module.exports = {
