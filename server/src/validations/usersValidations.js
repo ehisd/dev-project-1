562f714327b9b3a7999d39b698f51549ebfdd986
bhrayhaw <elijahapreko@gmail.com>
2024-03-02 19:19:10 +0000

    solved linting issues

--- server/src/validations/usersValidations.js
+++ server/src/validations/usersValidations.js
@@ -4,16 +4,17 @@ const Joi = require('joi');
 const validateUser = Joi.object({
   firstname: Joi.string().required(),
   lastname: Joi.string().required(),
-  username: Joi.string().alphanum().min(3).max(30).required(),
+  username: Joi.string().alphanum().min(3).max(30)
+    .required(),
   email: Joi.string().email().required(),
   password: Joi.string()
     .min(8)
     .max(40)
     .pattern(
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+{}|:"<>?~`\-=[\]\\;',./]).{8,40}$/
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+{}|:"<>?~`\-=[\]\\;',./]).{8,40}$/,
     )
     .message(
-      '"{#label}" must contain at least 1 lowercase letter, 1 uppercase letter, 1 number, 1 symbol, and be 8-40 characters.'
+      '"{#label}" must contain at least 1 lowercase letter, 1 uppercase letter, 1 number, 1 symbol, and be 8-40 characters.',
     )
     .required(),
 });
@@ -25,10 +26,10 @@ const validateLogin = Joi.object({
     .min(8)
     .max(40)
     .pattern(
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+{}|:"<>?~`\-=[\]\\;',./]).{8,40}$/
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+{}|:"<>?~`\-=[\]\\;',./]).{8,40}$/,
     )
     .message(
-      '"{#label}" must contain at least 1 lowercase letter, 1 uppercase letter, 1 number, 1 symbol, and be 8-40 characters.'
+      '"{#label}" must contain at least 1 lowercase letter, 1 uppercase letter, 1 number, 1 symbol, and be 8-40 characters.',
     )
     .required(),
 });
