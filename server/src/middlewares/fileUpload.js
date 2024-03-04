19e7f7b2fce7e7a6e3b41453542ac65681762587
bhrayhaw <elijahapreko@gmail.com>
2024-03-04 12:44:50 +0000

    multer config for handling image file

---
+++ server/src/middlewares/fileUpload.js
@@ -0,0 +1,16 @@
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/profiles');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+module.exports = {
+  uploadProfilePic: upload.single('profilePic'),
+};
